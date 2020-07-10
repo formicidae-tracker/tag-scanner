d95062af3b0f3400f45c83cd2e9551f451d31be1
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2020-07-10 12:39:10 +0200

    Sets the application name and organization

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,16 @@
+#include <QApplication>
 
-int main(int , char **) {
-	return 0;
+
+int main(int argc, char ** argv) {
+
+	QCoreApplication::setOrganizationName("FORmicidae Tracker");
+	QCoreApplication::setOrganizationDomain("formicidae-tracker.github.io");
+	QCoreApplication::setApplicationName("FORT Tag Scanner");
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+	QApplication fortTagScanner(argc,argv);
+
+
+
+	return fortTagScanner.exec();
 }
